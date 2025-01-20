20e8ae8b64f5709cfa836e4aeb7b7f0b03d06e2e
ashisnayak-18 <ashisranjannayak2002@gmail.com>
2025-01-20 22:45:26 +0530

    Updated cookies options

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,12 +1,10 @@
 export const sendToken = (user, statusCode, res, message) => {
     const token = user.getJWTToken();
     const options = {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
       httpOnly: true,
-      sameSite: 'lax', 
-      path: '/',
+      sameSite: 'none',
+      maxAge:process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
+      secure:true
     };
   
     res.status(statusCode).cookie("token", token, options).json({
